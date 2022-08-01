0944c73b11c4f4a9d7b7837499a747a253ebeb03
Ducminhhuy <ducminhhuy99tbvn2@gmail.com>
2022-08-01 09:52:40 +0700

    18

--- 1500.js
+++ 1500.js
@@ -1,3 +1,6 @@
+let img=["http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcR8gQcD5ewEV7b-FcPzhJ2u6OGX_lChJROgta0c6zyRPPLSe2ES6yKJQvxiQOoyjdhp",
+"https://cdn.britannica.com/05/147405-004-8572CF16.jpg",
+"http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcRXeXigzN_jYaaGuvF-Q3O-uiyzGOB3tBvTNDzjj8Gz3apO5VUF_W4PlMuGXlNN1lqF",]
 let sua=["Leonardo da Vinci (phát âm tiếng Ý: [leoˈnardo da ˈvintʃi] (nghe)[nb 1]; sinh ngày 15 tháng 4 năm 1452 - tại Anchiano, Ý, mất ngày 2 tháng 5 năm 1519 tại Amboise, Pháp), tên khai sinh là Leonardo di ser Piero da Vinci,[nb 2] là một họa sĩ, nhà điêu khắc, kiến trúc sư, nhạc sĩ, bác sĩ, kỹ sư, nhà giải phẫu, nhà phát minh và nhà triết học tự nhiên người Ý. Ông được coi là thiên tài toàn năng nhất lịch sử nhân loại. Ông là tác giả của những bức hoạ nổi tiếng như Mona Lisa, Bữa ăn tối cuối cùng.",
 "Roger Bacon, O.M. (1214–1294), cũng gọi là Doctor Mirabilis (tiếng Latin:thầy giáo tuyệt vời), là một trong những thầy dòng Franciscan nổi tiếng vào thời của ông. Ông là một triết gia người Anh đã nhấn mạnh tầm quan trọng đáng kể vào chủ nghĩa kinh nghiệm, ông là một trong những người châu Âu đầu tiên ủng hộ phương pháp khoa học hiện đại.",
 "Desiderius Erasmus Roterodamus (28 tháng 10 năm 1466 - 12 tháng 7 năm 1536), cũng gọi là Erasmus thành Rotterdam là nhà triết học chủ nghĩa nhân văn Phục Hưng phương Bắc,[1][2][3] linh mục Công giáo, nhà phê bình xã hội, giáo sư, nhà thần học người Hà Lan. Ông được xem như học giả lớn nhất trong thời đại của mình, nổi tiếng với các tác phẩm mang giọng điệu chế giễu sâu cay tầng lớp tăng lữ. Ông đã góp phần vào việc tuyển tập các bản văn Tân Ước tiếng Hy Lạp và tiếng La-tinh (bản Vulgata khoảng thế kỷ IV) trong đó mang những sắc thái nhân văn chủ nghĩa, bản Kinh Thánh này sau được Martin Luther dùng để dịch sang tiếng Đức. Kinh Thánh của ông có rất nhiều ảnh hưởng. Ông được gọi là Hoàng tử của chủ nghĩa nhân văn và là nhà tiên tri cho Cải cách Tin Lành."]
@@ -6,9 +9,11 @@ function show(){
     for(let i=0;i<sua.length;i++) {
         count += "<tr>" +
             "<td>" + sua[i] + "</td>" +
+            "<td><img src='"+img[i]+"' height='60px'></td>"+
             "<td onclick='edit(" + i + ")' bgcolor='#ffd700'>edit</td>" +
             "<td onClick='deleteIn(" + i + ")' bgcolor='aqua'>delete" + "</td>" +
             "<td onclick='save(" + i + ")' bgcolor='#6495ed' >save</td>" +
+
             "</tr>"
     }
     document.getElementById("enter1").innerHTML = count;
--- hientai.js
+++ hientai.js
@@ -1,3 +1,6 @@
+let img=["https://vcdn-vnexpress.vnecdn.net/2017/04/26/6-7047-1493192943.jpg",
+"http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcRsWLVF3JTFBj1_hnGNWFngZUmte1C0--rCCaw2jQ9_hvquZWtBMcEvZ2oQU2iqzFNi",
+"http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcQEa5vIB5rge7TBnRlfcRE3aenGyBmTuIVJFOxEFkEphZVxcaxDQkImgUzPPsBEAy7N"]
 let sua=["Albert Einstein (phiên âm tiếng Việt: An-be Anh-xtanh) (/ˈaɪnstaɪn/ EYEN-styne;[4] tiếng Đức: [ˈalbɛʁt ˈʔaɪnʃtaɪn]  (Speaker Icon.svg nghe); 14 tháng 3 năm 1879 – 18 tháng 4 năm 1955) là một nhà vật lý lý thuyết người Đức, được công nhận là một trong những nhà vật lý vĩ đại nhất mọi thời đại, người đã phát triển thuyết tương đối tổng quát, một trong hai trụ cột của vật lý hiện đại (trụ cột còn lại là cơ học lượng tử). Mặc dù được biết đến nhiều nhất qua phương trình về sự tương đương khối lượng-năng lượng được xem là phương trình nổi tiếng nhất thế giới,[5] ông lại được trao Giải Nobel Vật lý năm 1921 cho những cống hiến của ông đối với vật lý lý thuyết, và đặc biệt cho sự khám phá ra định luật của hiệu ứng quang điện.[6] Công trình về hiệu ứng quang điện của ông mang tính bước ngoặt khai sinh ra lý thuyết lượng tử.",
 "Thomas Alva Edison (11 tháng 2 năm 1847 – 18 tháng 10 năm 1931) là một nhà phát minh và thương nhân đã phát triển rất nhiều thiết bị có ảnh hưởng lớn tới cuộc sống trong thế kỷ 20. Ông được một nhà báo đặt danh hiệu Thầy phù thủy ở Menlo Park, ông là một trong những nhà phát minh đầu tiên ứng dụng các nguyên tắc sản xuất hàng loạt vào quy trình sáng tạo, và vì thế có thể coi là đã sáng tạo ra phòng nghiên cứu công nghiệp đầu tiên. Một số phát minh được gán cho ông, tuy ông không hoàn toàn là người đầu tiên có ý tưởng đó, nhưng sau khi bằng sáng chế đầu tiên được thay đổi nó trở thành của ông (nổi tiếng nhất là bóng đèn, trên thực tế là công việc của rất nhiều người bên trong công ty của ông). Edison được coi là một trong những nhà phát minh, nhà khoa học vĩ đại và giàu ý tưởng nhất trong lịch sử, ông giữ 1.093 bằng sáng chế tại Hoa Kỳ dưới tên ông, cũng như các bằng sáng chế ở Anh Quốc, Pháp, và Đức (tổng cộng 1.500 bằng phát minh trên toàn thế giới).",
 "Nikola Tesla (chữ Kirin Serbia: Никола Тесла) (10 tháng 7 năm 1856 – 7 tháng 1 năm 1943) là một nhà phát minh, kỹ sư điện, kỹ sư cơ khí,người Mỹ gốc Serbia.[2][3][4] Ông được biết đến nhiều nhất vì những đóng góp của mình để thiết kế ra hệ thống dòng điện xoay chiều cung cấp điện dòng điện xoay chiều hiện đại"]
@@ -6,6 +9,7 @@ function show(){
     for(let i=0;i<sua.length;i++) {
         count += "<tr>" +
             "<td>" + sua[i] + "</td>" +
+            "<td><img src='"+img[i]+"'height='50px'></td>"+
             "<td onclick='edit(" + i + ")' bgcolor='#ffd700'>edit</td>" +
             "<td onClick='deleteIn(" + i + ")' bgcolor='aqua'>delete" + "</td>" +
             "<td onclick='save(" + i + ")' bgcolor='#6495ed' >save</td>" +
